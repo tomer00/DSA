7cc6037133db96eca43f2628a622ee2b144853d1
tomer00gud <himanshutomer00@gmail.com>
2023-06-28 21:47:12 +0530

    addRec in bst

--- bst.cpp
+++ bst.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 
 class node {
+public:
     int val;
     node* left, * right;
-public:
     void addLeft(int val) {
         left = new node(val);
     }
@@ -19,19 +19,41 @@ public:
 };
 
 class BST {
-    node* root;
+    node* root = NULL;
+    node* insertRec(node* root, int val) {
+        if (root == NULL) return new node(val);
+        if (root->val < val) root->right = insertRec(root->right, val);
+        else root->left = insertRec(root->left, val);
+        return root;
+    }
+    void inorder(node* root) {
+        if (root == NULL) return;
+        inorder(root->left);
+        cout << root->val << " ";
+        inorder(root->right);
+    }
 public:
     void add(int val) {
-        if (root == NULL) root = new node(val);
-        else {
-
-        }
+        this->root = insertRec(root, val);
     }
     bool del(int val) {
-        
+    }
+    void printInorder() {
+        inorder(root);
+        cout << endl;
     }
 };
 
 int main() {
+
+    BST bst;
+    bst.add(15);
+    bst.add(1);
+    bst.add(5);
+    bst.add(3);
+    bst.add(8);
+
+    bst.printInorder(); //1 3 5 8 15
+
     return 0;
 }
