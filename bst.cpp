9690cee18cea96721add7adabd12f3e67dd466ab
tomer00gud <himanshutomer00@gmail.com>
2023-06-29 20:26:19 +0530

    addind iteratively

--- bst.cpp
+++ bst.cpp
@@ -23,7 +23,7 @@ class BST {
     node* insertRec(node* root, int val) {
         if (root == NULL) return new node(val);
         if (root->val < val) root->right = insertRec(root->right, val);
-        else root->left = insertRec(root->left, val);
+        else if (root->val > val) root->left = insertRec(root->left, val);
         return root;
     }
     void inorder(node* root) {
@@ -36,6 +36,23 @@ public:
     void add(int val) {
         this->root = insertRec(root, val);
     }
+    void addIterative(int val) {
+        if (root == NULL) root = new node(val);
+        else {
+            node* temp = root;
+            while (1) {
+                if (val == temp->val) break;
+                else if (temp->val < val) {
+                    if (temp->right == NULL) temp->addRight(val);
+                    else temp = temp->right;
+                }
+                else {
+                    if (temp->left == NULL) temp->addLeft(val);
+                    else temp = temp->left;
+                }
+            }
+        }
+    }
     bool del(int val) {
     }
     void printInorder() {
@@ -47,11 +64,11 @@ public:
 int main() {
 
     BST bst;
-    bst.add(15);
-    bst.add(1);
-    bst.add(5);
-    bst.add(3);
-    bst.add(8);
+    bst.addIterative(15);
+    bst.addIterative(1);
+    bst.addIterative(5);
+    bst.addIterative(3);
+    bst.addIterative(8);
 
     bst.printInorder(); //1 3 5 8 15
 
