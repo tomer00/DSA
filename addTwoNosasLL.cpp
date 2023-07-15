c3690630a8abdfa7768920848af3da54875e1643
tomer00gud <himanshutomer00@gmail.com>
2023-07-15 21:18:45 +0530

    add number as LL optimized with very long interger values using input as string

--- addTwoNosasLL.cpp
+++ addTwoNosasLL.cpp
@@ -64,19 +64,88 @@ node* sumLL(node* f, node* s) {
     return numberToLL(n1 + n2);
 }
 
+node* revLL(node* root) {
+    if (root == NULL || root->next == NULL) return root;
+    node* prev = NULL;
+    node* curr = root;
+    node* forward = root->next;
+    while (curr != NULL) {
+        forward = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = forward;
+    }
+    return prev;
+}
+
+//Approach 2
+//optimized
+node* sumLL2(node* f, node* s) {
+    // revese both LL
+    f = revLL(f);
+    s = revLL(s);
+    node* nLL;
+    int carr = 0;
+    int sum = f->data + s->data;
+    nLL = new node(sum % 10);
+    carr = sum / 10;
+    f = f->next;
+    s = s->next;
+    node* t = nLL;
+    while (f != NULL && s != NULL) {
+        sum = f->data + s->data + carr;
+        nLL->next = new node(sum % 10);
+        carr = sum / 10;
+        f = f->next;
+        s = s->next;
+        nLL = nLL->next;
+    }
+
+    f = f == NULL ? s : f;
+    while (f != NULL) {
+        sum = f->data + carr;
+        nLL->next = new node(sum % 10);
+        carr = sum / 10;
+        f = f->next;
+        nLL = nLL->next;
+    }
+    return revLL(t);
+}
+//taking input as string for a very long number
+node* stringTOLL(string& no) {
+    int nn = no.size();
+    if (nn == 1)
+        return new node(no[0] - '0');
+
+    node* n = new node(no[0] - '0');
+    node* t = n;
+
+    for (int i = 1;i < nn;i++) {
+        t->next = new node(no[i] - '0');
+        t = t->next;
+    }
+
+    return n;
+}
+
 int main() {
 
-    int n1, n2, n3 = 0;
+    // int n1, n2, n3 = 0;
     cout << "Enter Two Numbers to add : ";
-    cin >> n1 >> n2;
+    // cin >> n1 >> n2;
 
-    node* firstLL = numberToLL(n1);
-    node* secondLL = numberToLL(n2);
+    // node* firstLL = numberToLL(n1);
+    // node* secondLL = numberToLL(n2);
+
+    string n1, n2;
+    cin>>n1>>n2;
+    node* firstLL = stringTOLL(n1);
+    node* secondLL = stringTOLL(n2);
 
     printLL(firstLL);
     printLL(secondLL);
 
-    printLL(sumLL(firstLL,secondLL));
+    printLL(sumLL2(firstLL, secondLL));
 
     return 0;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+ode* secondLL = numberToLL(n2);
