aa653f24f05d5ccdac71bcc8b96a19ca73ef3eb0
tomer00gud <himanshutomer00@gmail.com>
2023-08-06 21:17:16 +0530

    quickSort

--- Searching ans Sorting/quickSort.cpp
+++ Searching ans Sorting/quickSort.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 using namespace std;
 
-
-void quickSort(int *arr,int n){
-    
+int findPivot(int* arr, int s, int e) {
+    int pivot = arr[s];
+    int i = s, j = e;
+    while (i < j) {
+        while (arr[i] <= pivot && i < e) i++;
+        while (arr[j] > pivot && j > s) j--;
+        if (i < j) swap(arr[i], arr[j]);
+    }
+    swap(arr[j],arr[s]);
+    return j;
+}
+void quickSort(int* arr, int s, int e) {
+    if (s < e) {
+        int pivot = findPivot(arr, s, e);
+        quickSort(arr, s, pivot - 1);
+        quickSort(arr, pivot + 1, e);
+    }
 }
 
 void printArr(int* arr, int n) {
@@ -20,7 +34,7 @@ int main() {
     cout << "Enter Space separated elements : ";
     for (int i = 0;i < n;i++)
         cin >> arr[i];
-    quickSort(arr, n);
+    quickSort(arr, 0, n - 1);
     printArr(arr, n);
     return 0;
 }
